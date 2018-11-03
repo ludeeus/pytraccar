ba40ac1364184b06865b6856f775191494d892aa
ludeeus <joasoe@gmail.com>
2018-11-03 12:24:01 +0100

    Versionbump

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ from traccar import API
 URL = 'http://192.168.2.11:8082/api'
 
 async def test():
-    """Example usage of pyversion."""
+    """Example usage of pytraccar."""
     async with aiohttp.ClientSession() as session:
         data = API(LOOP, session, 'admin', 'admin', URL)
         await data.get_geofences()
