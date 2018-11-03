cbe2ccd4a404ee0880772f7253a997a47215cec4
ludeeus <joasoe@gmail.com>
2018-11-03 12:22:50 +0100

    Added get_devices and get_geofences

--- example.py
+++ example.py
@@ -1,15 +1,20 @@
 """Example usage of pytraccar."""
 import asyncio
 import aiohttp
-from pytraccar.api import API
+from traccar import API
 
-LOOP = asyncio.get_event_loop()
-
+URL = 'http://192.168.2.11:8082/api'
 
 async def test():
-    """Example usage of pytraccar."""
+    """Example usage of pyversion."""
     async with aiohttp.ClientSession() as session:
-        data = API(LOOP, session)
-        print(data.test)
+        data = API(LOOP, session, 'admin', 'admin', URL)
+        await data.get_geofences()
+        await data.get_devices()
 
+        print("Geofences:", data.geofences)
+        print("Devices:", data.devices)
+
+
+LOOP = asyncio.get_event_loop()
 LOOP.run_until_complete(test())
--- pytraccar/api.py
+++ pytraccar/api.py
@@ -4,21 +4,59 @@ Update and fetch device information from Traccar.
 This code is released under the terms of the MIT license. See the LICENSE
 file for more details.
 """
+import asyncio
+import logging
+import socket
+
+import aiohttp
+import async_timeout
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class API(object):
     """A class for the Traccar API."""
 
-    def __init__(self, loop, session):
+    def __init__(self, loop, session, username, password, api_endpoint):
         """Initialize the class."""
         self._loop = loop
+        self._auth = aiohttp.BasicAuth(username, password)
+        self._api = api_endpoint
         self._session = session
+        self._geofences = {}
+        self._devices = []
 
-    async def dummy(self):
+    async def get_geofences(self):
         """Get the local installed version."""
-        print("For now this is just a placeholder")
+        base_url = self._api + '/geofences'
+        try:
+            async with async_timeout.timeout(5, loop=self._loop):
+                response = await self._session.get(base_url, auth=self._auth)
+            data = await response.json()
+            for geofence in data or []:
+                self._geofences[geofence['id']] = geofence['name']
+        except (asyncio.TimeoutError,
+                aiohttp.ClientError, socket.gaierror) as error:
+            _LOGGER.error('Error fetching data from Traccar, %s', error)
+
+    async def get_devices(self):
+        """Get the local installed version."""
+        base_url = self._api + '/devices'
+        try:
+            async with async_timeout.timeout(5, loop=self._loop):
+                response = await self._session.get(base_url, auth=self._auth)
+            data = await response.json()
+            self._devices = data
+        except (asyncio.TimeoutError,
+                aiohttp.ClientError, socket.gaierror) as error:
+            _LOGGER.error('Error fetching data from Traccar, %s', error)
 
     @property
-    def test(self):
-        """Return sample text."""
-        return "For now this is just a placeholder."
+    def geofences(self):
+        """Return the configured geofences if any."""
+        return self._geofences
+
+    @property
+    def devices(self):
+        """Return the devices if any."""
+        return self._devices
