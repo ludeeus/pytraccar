0eec12667fad4bf75009332295af9c99313ba799
ludeeus <joasoe@gmail.com>
2018-12-30 11:48:39 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="pytraccar",
-    version="0.2.0",
+    version="0.2.1",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
