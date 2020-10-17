188c51f5945cd08f356285f5d14735d70b407038
Ville Skyttä <ville.skytta@iki.fi>
2020-10-18 00:06:28 +0300

    Exclude tests from installs (#9)
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     tests_require=["pytest", "aiohttp", "pytest-asyncio", "aresponses"],
     long_description_content_type="text/markdown",
     url="https://github.com/ludeeus/pytraccar",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests.*", "tests"]),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
