46506477ad2622f2d2b0adf174a54177a6239416
Joakim Sørensen <joasoe@gmail.com>
2024-02-11 10:13:42 +0100

    Adjust example (#55)

--- example.py
+++ example.py
@@ -1,22 +1,39 @@
 """Example usage of pytraccar."""
 import asyncio
+import logging
+import os
 
 import aiohttp
 
 from pytraccar import ApiClient
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler()],
+)
 
-async def test():
+
+async def test() -> None:
     """Example usage of pytraccar."""
-    async with aiohttp.ClientSession() as client_session:
+    async with aiohttp.ClientSession(
+        cookie_jar=aiohttp.CookieJar(unsafe=True)
+    ) as client_session:
         client = ApiClient(
-            host="127.0.0.1",
-            username="admin",
-            password="Password123!",
+            host=os.environ["TRACCAR_HOST"],
+            port=os.environ.get("TRACCAR_PORT", 8082),
+            username=os.environ["TRACCAR_USERNAME"],
+            password=os.environ["TRACCAR_PASSWORD"],
             client_session=client_session,
         )
-        print(await client.get_server())
+        server = await client.get_server()
+        logging.info(
+            "Connected to Traccar server (%s:%s) which is running version %s",
+            os.environ["TRACCAR_HOST"],
+            os.environ.get("TRACCAR_PORT", 8082),
+            server["version"],
+        )
 
 
-LOOP = asyncio.get_event_loop()
-LOOP.run_until_complete(test())
+asyncio.run(test())
